cb0d049c1c3adfd74f46b09f6f2f8458ce44115d
Reza-mohseni <mohseni.rezaofficial@gmail.com>
2025-03-20 16:38:41 +0330

    تمرین شروط چند گانه

--- practic.js
+++ practic.js
@@ -3,9 +3,22 @@
 // var age=1404-yars;
 // console.log(age);
 //تمرین شروط
-var age = +prompt("لطفا سن خود را وارد کنید");
-if(age >=18){
-    console.log("شما دسترسی کامل دارید ");
-}else{
-    console.log("شما دسترسی ندارید");
-}
+// var age = +prompt("لطفا سن خود را وارد کنید");
+// if(age >=18){
+//     console.log("شما دسترسی کامل دارید ");
+// }else{
+//     console.log("شما دسترسی ندارید");
+// }
+
+//تمرین شروط چند گانه
+// var score=+prompt("نمره خود را وارد کنید");
+// if(score>18&&score<=20){
+//     console.log('عالی');
+// }else if(score>=15 && score <18){
+//     console.log("خیلی خوب");
+// }else if(score >=10 && score<15){
+//     console.log("خوب");
+// }else if (score >=5 && score<10 ){
+//     console.log("ضعیف");
+// }
+
