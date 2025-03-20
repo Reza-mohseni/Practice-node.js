d3edd2eb83aecf3d5dc1dcfa5f9a4a0fcde0e8f7
Reza-mohseni <mohseni.rezaofficial@gmail.com>
2025-03-20 16:30:14 +0330

    ترمین شروط

--- practic.js
+++ practic.js
@@ -1,3 +1,11 @@
-var yars=+prompt("سال تولد خود را وارد کنید");
-var age=1404-yars;
-console.log(age);
+//تمرین محاسبه سن
+// var yars=+prompt("سال تولد خود را وارد کنید");
+// var age=1404-yars;
+// console.log(age);
+//تمرین شروط
+var age = +prompt("لطفا سن خود را وارد کنید");
+if(age >=18){
+    console.log("شما دسترسی کامل دارید ");
+}else{
+    console.log("شما دسترسی ندارید");
+}
